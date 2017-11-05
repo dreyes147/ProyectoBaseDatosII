57e029df565fa74aa7ddc4eb07a74445d4d7b5a5
Josue Avendaño <jragutierrez97@hotmail.com>
2017-11-04 22:47:03 -0600

    Clases de Reportes en proceso

---
+++ CapaDatos/Clases/ClsReportes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos.Clases
+{
+    public class ClsReportes
+    {
+        Conexion.ClsConexion enlaceWeb = new Conexion.ClsConexion();
+        public void InsertarRepo(string id, string Tipo)
+        {
+            string vInstruccion = "Insert Into ReporteSnip(Id_Snip, Descripcion) values(" + id + ", '" + Tipo + "')";
+            enlaceWeb.Ejecutar(vInstruccion, "BaseDatos");
+        }
+    }
+}
---
+++ CapaNegocios/ClsReportes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaNegocios
+{
+    class ClsReportes
+    {
+        CapaDatos.Clases.ClsReportes enlace = new CapaDatos.Clases.ClsReportes();
+        public void inserReporte(string id, string Tipo)
+        {
+            enlace.InsertarRepo(id,Tipo);
+        }
+    }
+}
