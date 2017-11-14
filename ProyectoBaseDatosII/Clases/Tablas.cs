abb0b96ef904ee8624090406732296146fd2cb57
dreyes147 <davidre701@gmail.com>
2017-11-14 11:08:48 -0600

    ,

---
+++ ProyectoBaseDatosII/Clases/Tablas.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoBaseDatosII.Clases
+{
+    class Tablas
+    {
+        #region Declaracion de variables
+        private string vNombreTabla;
+        private string vAliasTabla;
+        private string vNombreCampo;
+        #endregion
+
+        #region Declaracion de constructor
+
+        public Tablas() {
+            vNombreTabla = string.Empty;
+            vAliasTabla = string.Empty;
+            vNombreCampo = string.Empty;
+        }
+        #endregion
+
+        #region Declaracion de propiedades
+        public string NombreTabla {
+            get
+            {
+                return vNombreTabla;
+            }
+            set
+            {
+                vNombreTabla = value;
+            }
+        }
+        public string AliasTabla
+        {
+            get
+            {
+                return vAliasTabla;
+            }
+            set
+            {
+                vAliasTabla = value;
+            }
+        }
+        public string NombreCampo
+        {
+            get
+            {
+                return vNombreCampo;
+            }
+            set
+            {
+                vNombreCampo = value;
+            }
+        }
+        #endregion
+    }
+}
